ce51efb30017375ecca34756c8dcf26d9473ed76
lianglele <liangle1986@126.com>
2019-12-06 20:46:36 +0800

    物物网管理平台

--- mywuwu-visual/mywuwu-gecco/src/main/java/com/mywuwu/gecco/GeccoApplication.java
+++ mywuwu-visual/mywuwu-gecco/src/main/java/com/mywuwu/gecco/GeccoApplication.java
@@ -1,9 +1,15 @@
 package com.mywuwu.gecco;
 
+import com.mywuwu.pigx.common.security.annotation.EnablePigxFeignClients;
+import com.mywuwu.pigx.common.security.annotation.EnablePigxResourceServer;
+import com.mywuwu.pigx.common.swagger.annotation.EnablePigxSwagger2;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
+@EnablePigxSwagger2
+@SpringCloudApplication
+@EnablePigxFeignClients
+@EnablePigxResourceServer
 public class GeccoApplication {
 
 	public static void main(String[] args) {
