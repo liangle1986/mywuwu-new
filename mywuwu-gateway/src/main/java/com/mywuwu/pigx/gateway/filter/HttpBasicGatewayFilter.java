88d32a341dac7e81db134a757217c3a6abf4bb0b
lianglele <liangle1986@126.com>
2019-12-21 21:30:38 +0800

    物物网管理平台

--- mywuwu-gateway/src/main/java/com/mywuwu/pigx/gateway/filter/HttpBasicGatewayFilter.java
+++ mywuwu-gateway/src/main/java/com/mywuwu/pigx/gateway/filter/HttpBasicGatewayFilter.java
@@ -44,7 +44,7 @@ public class HttpBasicGatewayFilter extends AbstractGatewayFilterFactory {
 			} else {
 				ServerHttpResponse response = exchange.getResponse();
 				response.setStatusCode(HttpStatus.UNAUTHORIZED);
-				response.getHeaders().add(HttpHeaders.WWW_AUTHENTICATE, "Basic Realm=\"pigx\"");
+				response.getHeaders().add(HttpHeaders.WWW_AUTHENTICATE, "Basic Realm=\"mywuwu\"");
 				return response.setComplete();
 			}
 		};
